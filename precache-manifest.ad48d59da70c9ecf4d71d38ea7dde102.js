08aa251a57261d05153a36ab0d7b774e7a96d957
Colin Brayton <gringolalia@gmail.com>
2020-07-29 13:01:47 -0300

    deploy

---
+++ precache-manifest.ad48d59da70c9ecf4d71d38ea7dde102.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b648272b488567611f9",
+    "url": "/css/about.8b304b3a.css"
+  },
+  {
+    "revision": "2531e304851e6dac90e6",
+    "url": "/css/app.8ac06335.css"
+  },
+  {
+    "revision": "9b52053e6b990f377781",
+    "url": "/css/chunk-vendors.036b6237.css"
+  },
+  {
+    "revision": "fcfbb86730b77fd71218f617e1c56a98",
+    "url": "/img/hindenburger.fcfbb867.jpg"
+  },
+  {
+    "revision": "9b6200320cf7c0cc32f17498808d2108",
+    "url": "/img/ponteavmarinho.9b620032.jpg"
+  },
+  {
+    "revision": "279dcec949579055f7238a2bf5eb00a4",
+    "url": "/img/vintage-test-two.279dcec9.jpeg"
+  },
+  {
+    "revision": "a8690c18392d93683d918a1f5a926564",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9b648272b488567611f9",
+    "url": "/js/about.f796cdf3.js"
+  },
+  {
+    "revision": "2531e304851e6dac90e6",
+    "url": "/js/app.b33c4710.js"
+  },
+  {
+    "revision": "9b52053e6b990f377781",
+    "url": "/js/chunk-vendors.a17b4046.js"
+  },
+  {
+    "revision": "ddbabb9e0e1fc778fa7189f8c73ad0ca",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
